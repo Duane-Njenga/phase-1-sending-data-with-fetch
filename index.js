723091393d8e75619c5c094eac6489337471a763
Dwayne <dune.njenga@student.moringaschool.com>
2025-03-20 23:21:33 +0300

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+function submitData(userName, userEmail){
+   return fetch(`http://localhost:3000/users`,{
+        method : "POST",
+        headers : {
+            "Content-Type" : "application/json",
+            "Accept" : "application/json"
+        },
+        body: JSON.stringify({
+            name : `${userName}`,
+            email : `${userEmail}`,
+        }),
+    }).then(res => res.json())
+    .then(data => {
+        console.log(data);
+        document.body.innerHTML = `<p>${data.id}</p>`
+    })
+    .catch(error => {
+        console.log('Error:', error);
+        document.body.innerHTML = `<p>${error}}</p>`
+        alert(error)
+        
+    })
+}
+
